8baaba854b414d5bde22619cea72dbaa6d85ff77
Gao Guanbin <gaoguanbin110@gmail.com>
2020-12-05 21:21:24 +0100

    add code for generating the clean dataset

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # cleandataassignment
+
+the code first read in the features file to provide the variable names,
+
+then read in training data,
+
+the x_train file contain most of the data, the y_train and subject_train contains subject information and activeiy information, 
+then combine these three we got the training dataset, finally add the variable names
+
+then same is done with the test data.
+
+Then combining the training and test data together by rbind function to generate data.all. 
+
+Then extract the columns from data.all that contains either mean or std in the varibale names to form data.sub
+
+In order to label the activity with descriptive names, the activity_labels file is read in. Attach the activity to the first columns of this activity_label file, merge is used to combining the data.sub and activity_labels together by the common column "activity". 
+This way the one more column called "activity names" is added to the data. 
+
+The change the names of the varibale by using gsub function.
+
+finally, independent tidy data set with the average of each variable for each activity and each subject is created by aggregate function and written out by write.csv function. 
---
+++ run_analysis.R
@@ -0,0 +1,52 @@
+library(data.table)
+features <- read.table('./UCI HAR Dataset/features.txt', header = FALSE, sep = ' ')
+features <- as.character(features[,2])
+##read features(future variable names)
+
+data.train.x <- read.table('./UCI HAR Dataset/train/X_train.txt')
+data.train.activity <- read.table('./UCI HAR Dataset/train/y_train.txt', header = FALSE, sep = ' ')
+data.train.subject <- read.table('./UCI HAR Dataset/train/subject_train.txt',header = FALSE, sep = ' ')
+##read the activity, subject and xtrain in
+
+data.train <-  data.frame(data.train.subject, data.train.activity, data.train.x)
+##combining them
+
+names(data.train) <- c(c('subject', 'activity'), features)
+##attach the names to the variables 
+
+data.test.x <- read.table('./UCI HAR Dataset/test/X_test.txt')
+data.test.activity <- read.table('./UCI HAR Dataset/test/y_test.txt', header = FALSE, sep = ' ')
+data.test.subject <- read.table('./UCI HAR Dataset/test/subject_test.txt', header = FALSE, sep = ' ')
+
+data.test <-  data.frame(data.test.subject, data.test.activity, data.test.x)
+names(data.test) <- c(c('subject', 'activity'), features)
+##same thing for the test data, reading in and label with varibale names
+
+data.all <- rbind(data.train, data.test)
+##merge the training and test data together 
+
+mean_std.select <- grep('mean|std', names(data.all))
+##return the number of the columns that with names containing mean or std
+data.sub <- data.all[,c(1,2,mean_std.select)]
+##select the columns with mean or std in the variable names
+
+activity.labels <- read.table('./UCI HAR Dataset/activity_labels.txt', header = FALSE)
+names(activity.labels)<-c("activity","activity names")
+data.sub<-merge(x=activity.labels,y=data.sub,by="activity")
+##merge the activity labels with data.sub so that a new column of activity name is added
+
+name.new <- names(data.sub)
+name.new <- gsub("[(][)]", "", name.new)
+name.new <- gsub("^t", "TimeDomain_", name.new)
+name.new <- gsub("^f", "FrequencyDomain_", name.new)
+name.new <- gsub("Acc", "Accelerometer", name.new)
+name.new <- gsub("Gyro", "Gyroscope", name.new)
+name.new <- gsub("Mag", "Magnitude", name.new)
+name.new <- gsub("-mean-", "_Mean_", name.new)
+name.new <- gsub("-std-", "_StandardDeviation_", name.new)
+name.new <- gsub("-", "_", name.new)
+names(data.sub) <- name.new
+##Change names to appropriate ones
+
+data.tidy <- aggregate(data.sub[,4:82], by = list(activity = data.sub$"activity names", subject = data.sub$subject),FUN = mean)
+write.csv(data.tidy, file = "data_tidy.csv")
